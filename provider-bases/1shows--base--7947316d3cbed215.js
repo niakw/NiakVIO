bc41fe2907c30a42dedb1c85fde82b63a8e4afa2
NiakVIO ProviderBase migration <actions@users.noreply.github.com>
2026-08-28 23:39:01 +0000

    chore(providers): materialize durable ProviderBase store

---
+++ provider-bases/1shows--base--7947316d3cbed215.js
@@ -0,0 +1,2 @@
+/* NUVIO_PROVIDER_QUARANTINE_V1: catalogue_audit_playable_identity_contradiction */
+"use strict";async function getStreams(){return[]}typeof globalThis!="undefined"&&(globalThis.getStreams=getStreams);typeof module!="undefined"&&module&&module.exports&&(module.exports={getStreams});
---
+++ provider-bases/topcartoons--base--5c86d4cfbbe28bd9.js
@@ -0,0 +1,3 @@
+/* NUVIO_PROVIDER_QUARANTINE_V1: cross_title_content_mismatch */
+"use strict";
+/* NUVIO_PROVIDER_SECURITY_HARDENING_V1:77e110a2a7b7 */var __nuvioProviderSilentLog=function(){};async function getStreams(){return[]}if(typeof globalThis!=="undefined")globalThis.getStreams=getStreams;if(typeof module!=="undefined"&&module&&module.exports)module.exports={getStreams};
